500e17c4e62133dd5fcf213c640bbd731b7a2fb3
Kaitlyn Nowicki <katie.nowicki024@gmail.com>
2023-11-15 20:20:10 -0500

    lab review

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Your code here
 const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
 
 document.addEventListener("keydown",function (event) {
     if (event.key === "ArrowLeft") {
